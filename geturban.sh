54d9d52a4773c02903e08a21f0448dc6e75653c9
grobe0ba <grobe0ba>
2015-07-11 20:53:37 +0000

    8e358af6-00c5-11e4-9857-0015178af5da

--- geturban.sh
+++ geturban.sh
@@ -13,11 +13,14 @@ function geturban
 		fi
 		if `grep -q '/div' <(echo "$CLINE")`; then
 			unset DOPRNT
+			DIDPRNT=1
 		fi
 		if [ -n "$DOPRNT" ]; then
-			echo -en " " >&10
-			$HOME/src/scripts/commode/stripHTML.sed <(echo "$CLINE" | sed -e "s/<br[/]*>/ /g" -e "s/\&quot\;/'/g" -e "s/&#39;/'/g" | tr -d '\n') | tr -d '\n' | tr -cd '[:print:]' >&10
-			echo -en "\r\n" >&10
+			if [ -z $DIDPRNT ]; then
+				echo -en " " >&10
+				$HOME/src/scripts/commode/stripHTML.sed <(echo "$CLINE" | sed -e "s/<br[/]*>/ /g" -e "s/\&quot\;/'/g" -e "s/&#39;/'/g" | tr -d '\n') | tr -d '\n' | tr -cd '[:print:]' >&10
+				echo -en "\r\n" >&10
+			fi
 		fi
 	done
 	exit
