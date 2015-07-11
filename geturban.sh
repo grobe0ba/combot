204adf7bce3616a3d1cc0efd147a91d076dca763
grobe0ba <grobe0ba>
2015-07-11 20:53:27 +0000

    e0eab6be-007a-11e4-9857-0015178af5da

--- geturban.sh
+++ geturban.sh
@@ -1,17 +1,23 @@
 #!/usr/local/bin/bash
 
-unset OLINE
+function geturban
+{
 
-lynx --source  http://www.urbandictionary.com/define.php?term=PRN |
-while read CLINE
-do
-	if `grep -q "class='meaning'" <(echo "$CLINE")`; then
-		DOPRNT=1
-	fi
-	if `grep -q '/div' <(echo "$CLINE")`; then
-		unset DOPRNT
-	fi
-	if [ -n "$DOPRNT" ]; then
-		./stripHTML.sed <(echo "$CLINE" | tr -d '\n') | tr -d '\n'
-	fi
-done
+	unset OLINE
+
+	lynx --source  "http://www.urbandictionary.com/define.php?term=$1" |
+	while read CLINE
+	do
+		if `grep -q "class='meaning'" <(echo "$CLINE")`; then
+			DOPRNT=1
+		fi
+		if `grep -q '/div' <(echo "$CLINE")`; then
+			unset DOPRNT
+		fi
+		if [ -n "$DOPRNT" ]; then
+			./stripHTML.sed <(echo "$CLINE" | tr -d '\n') | tr -d '\n' | tr -cd '[:print:]'
+			echo -en " "
+		fi
+	done
+	echo -en "\n"
+}
