bdaa7e351f3665cfe366cd04765df2cb7656f350
grobe0ba <grobe0ba>
2015-07-11 20:56:39 +0000

    fa27b34e-e227-437e-b8dc-5c894ab39db6

--- urbandictionary.sh
+++ urbandictionary.sh
@@ -2,9 +2,10 @@
 
 function urbandict_lookup
 {
-    TERM="$(echo "${1}" | sed -e 's/ /\%20/g')"
+    RTERM="${1}"
+    TERM="$(echo "${RTERM}" | sed -e 's/ /\%20/g')"
     URL="http://www.urbandictionary.com/define.php?term=${TERM}"
     #echo -en "L${URL}\r\nUrban Dictionary: ${1}\r\n" >&10
     OLINE="$(curl "${URL}" | grep -e "og:description" | ./udfilter)"
-    msg_out "Urban Dictionary on '${TERM}': ${OLINE}"
+    msg_out "Urban Dictionary on '${RTERM}': ${OLINE}"
 }
