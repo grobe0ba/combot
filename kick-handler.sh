0fc6e1f0866044f77f18faca010a9c37a9c42729
grobe0ba <grobe0ba>
2015-07-11 20:55:35 +0000

    Some more functionality.

--- kick-handler.sh
+++ kick-handler.sh
@@ -1,8 +1,18 @@
 #!/usr/pkg/bin/bash
 
+#[20:11:23] KICK: bag@faeroes needs approval to kick grobe0ba@iceland out of library
+
+WHO="$(echo "${LINE}" | cut -d' ' -f7)"
 REQ="$(echo "${LINE}" | cut -d' ' -f2)"
 
-if [ "${REQ}" == "${OWNER}" ];
+if [ "${WHO}" != "${OWNER}" ];
 then
-    kick_approve
+    for p in $(xargs < ./kicker);
+    do
+	if [ "$(echo ${REQ} | cut -d'@' -f1)" == "${P}" ];
+	then
+	    kick_approve &
+	    break
+	fi
+    done
 fi
