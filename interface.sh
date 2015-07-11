4f4111d4d7c50e9fb5e2d48ecb4ad6015397780a
grobe0ba <grobe0ba>
2015-07-11 20:55:29 +0000

    More fixing.

--- interface.sh
+++ interface.sh
@@ -2,20 +2,20 @@
 
 function msg
 {
-    LINE=$1
-    RLINE=$1
+    LINE="${1}"
+    RLINE="${1}"
 
-    PERSON=$(echo "$LINE" | cut -d' ' -f1 | tr -cd "[:alnum:]@")
-    LINE=$(echo "$LINE" | cut -d' ' -f2- | sed -e 's/^  *//' | tr -d "[:cntrl:]")
+    PERSON="$(echo "$LINE" | cut -d' ' -f1 | tr -cd "[:alnum:]@")"
+    LINE="$(echo "${LINE}" | cut -d' ' -f2- | sed -e 's/^  *//' | tr -d "[:cntrl:]")"
 
-    if [ "$PERSON" == "$OWNER" ];
+    if [ "${PERSON}" == "${OWNER}" ];
     then
 	. ./owner-commands.sh
     else
 	. ./public-commands.sh
     fi
 
-    if $(echo "$LINE" | grep -q '^KICK:');
+    if $(echo "${LINE}" | grep -q '^KICK:');
     then
 	. ./kick-handler.sh
     fi
@@ -25,31 +25,31 @@ function msg
 
 function key_out
 {
-    echo -en "$1\r\n" >&10
+    echo -en "${1}\r\n" >&10
     return
 }
 
 function msg_out
 {
-    echo -en " $1\r\n" >&10
+    echo -en " ${1}\r\n" >&10
     return
 }
 
 function pm_out
 {
-    echo -en "s$1\r\n$2\r\n" >&10
+    echo -en "s${1}\r\n${2}\r\n" >&10
     return
 }
 
 function emote
 {
-    echo -en "e$1\r\n" >&10
+    echo -en "e${1}\r\n" >&10
     return
 }
 
 function ch_sw
 {
-    echo -en "g$1\r\n" >&10
+    echo -en "g${1}\r\n" >&10
     return
 }
 
