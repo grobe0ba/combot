45e19c9e3f080adc85b83627fa1ff47fba92ded7
grobe0ba <grobe0ba>
2015-07-11 20:56:56 +0000

    886b1510-a081-4366-a73a-7a4ec5cb54fd

--- public-commands.sh
+++ public-commands.sh
@@ -34,8 +34,8 @@ case "${COMMAND}" in
 	MSG_SENT=1
 	;;
     wadd)
-	#	whois_add "${ARGUMENTS}";
-	msg_out "Please contact grobe0ba for cvs information to modify WHOIS database."
+	whois_add "${ARGUMENTS}";
+	#msg_out "Please contact grobe0ba for cvs information to modify WHOIS database."
 	;;
     whois)
 	whois_list "${ARGUMENTS}";
