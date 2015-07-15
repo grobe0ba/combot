d94bf3ff613f52425518b5c5710fa82c95495f7b
grobe0ba <grobe0ba>
2015-07-15 06:11:10 +0000

    screw wget and it's fucked up flags

--- urbandictionary.sh
+++ urbandictionary.sh
@@ -6,6 +6,6 @@ function urbandict_lookup
     TERM="$(echo "${RTERM}" | sed -e 's/ /\%20/g')"
     URL="http://www.urbandictionary.com/define.php?term=${TERM}"
     #echo -en "L${URL}\r\nUrban Dictionary: ${1}\r\n" >&10
-    OLINE="$(wget -o - "${URL}" | grep -e "og:description" | ./udfilter)"
+    OLINE="$(wget -O - "${URL}" | grep -e "og:description" | ./udfilter)"
     msg_out "Urban Dictionary on '${RTERM}': ${OLINE}"
 }
