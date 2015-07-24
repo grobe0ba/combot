c5f2f907b9de4f2b0a330199cf4ec032e025a33e
grobe0ba <grobe0ba>
2015-07-24 23:14:42 +0000

    Fixed a typo that caused it to always reuse the first term used

--- urbandictionary.sh
+++ urbandictionary.sh
@@ -5,7 +5,7 @@
 function urbandict_lookup
 {
     RTERM="${1}"
-    TERM="${TERM// /%20}"
+    TERM="${RTERM// /%20}"
     URL="http://www.urbandictionary.com/define.php?term=${TERM}"
     #echo -en "L${URL}\r\nUrban Dictionary: ${1}\r\n" >&10
     OLINE="$(wget -O - "${URL}" | grep -e "og:description" | ./udfilter)"
